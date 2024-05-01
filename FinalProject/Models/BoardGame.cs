91e7d059b633cc3b28d5375087037d3e1611fc0c
kylan120 <142834131+kylan120@users.noreply.github.com>
2024-05-01 10:08:38 -0500

    entered some validation for our project

--- FinalProject/Models/BoardGame.cs
+++ FinalProject/Models/BoardGame.cs
@@ -6,10 +6,16 @@ namespace FinalProject.Models
     public class BoardGame
     {
         [Key]
-        public int ID { get; set; } 
-        public string Name { get; set; }  
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Please enter a boardgame")]
+        [StringLength(30)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter a company")]
+        [StringLength (30)]
         public string Company {  get; set; }
-        public int Players { get; set; }  
+        [Range(1,8, ErrorMessage = "Please enter player count of 1-8")]
+        public int Players { get; set; }
+        
         public string Image {  get; set; }
         [DataType(DataType.Currency)]
         public double Price { get; set; }   
